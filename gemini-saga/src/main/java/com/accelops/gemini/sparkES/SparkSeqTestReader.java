2a3fd414774489c87083499b430431cceec7bf03
A-merpact <kaizhang.kun@gmail.com>
2015-12-29 16:52:41 -0800

    POM

--- gemini-saga/src/main/java/com/accelops/gemini/sparkES/SparkSeqTestReader.java
+++ gemini-saga/src/main/java/com/accelops/gemini/sparkES/SparkSeqTestReader.java
@@ -1,12 +1,13 @@
 package com.accelops.gemini.sparkES;
 
-import org.apache.hadoop.io.ByteWritable;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 
 import java.util.List;
 
@@ -24,17 +25,18 @@ public class SparkSeqTestReader {
         //String filePath = args[0];
 
         SparkConf sparkConf = new SparkConf().setAppName("TestReader");
+        sparkConf.set("es.index.auto.create", "true");
+        sparkConf.set("es.nodes", "10.0.0.138:9200,10.0.0.139:9200");
+
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
         JavaPairRDD<LongWritable, BytesWritable> timeEventRDD = sc.sequenceFile(args[0], LongWritable.class, BytesWritable.class);
-        List<String> bytesWritables = timeEventRDD.values().map(new Function<BytesWritable, String>() {
+        JavaRDD<String> stringRDD = timeEventRDD.values().map(new Function<BytesWritable, String>() {
             public String call(BytesWritable bytesWritable) {
                 return new String(bytesWritable.getBytes(), 0, bytesWritable.getLength());
             }
-        }).take(10);
+        });
 
-        for(String bw : bytesWritables) {
-            System.out.println(bw);
-        }
+        JavaEsSpark.saveJsonToEs(stringRDD, "accelops-2015.12.22");
 
 
     }
