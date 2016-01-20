26777ae9eede211afef57819972418371ac076aa
A-merpact <kaizhang.kun@gmail.com>
2016-01-20 11:41:16 -0800

    Set app args

--- gemini-saga/src/main/java/com/accelops/gemini/sparkES/SparkReaderLauncher.java
+++ gemini-saga/src/main/java/com/accelops/gemini/sparkES/SparkReaderLauncher.java
@@ -24,12 +24,12 @@ public class SparkReaderLauncher {
                     .setMainClass("com.accelops.gemini.sparkES.SparkSeqTestReader")
                     .setMaster("spark://ec2-52-33-244-102.us-west-2.compute.amazonaws.com:7077")
                     .setConf(SparkLauncher.DRIVER_MEMORY, "8g")
-                    .setConf(SparkLauncher.EXECUTOR_EXTRA_JAVA_OPTIONS, args[0])
+                    .addAppArgs(args[0])
                     .startApplication();
 
             while(!handle.getState().isFinal()) {
                 System.out.println("Job running: "  + handle.getState().name());
-                Thread.sleep(10);
+                Thread.sleep(3000);
             }
 
             System.out.println("Job finished: " + handle.getState().name());
